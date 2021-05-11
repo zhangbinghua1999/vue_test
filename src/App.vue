edda015edaeda25d133a111c2a4a0c43598fa80b
zhangbinghua1999 <690227436@qq.com>
2021-05-11 16:40:20 +0800

    新创建了box 组件

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,20 @@
 <template>
   <div id="app">
+    <Box/>
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
+import Box from './components/Box.vue'
 import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+    Box
   }
 }
 </script>
---
+++ src/components/Box.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="box">
+
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+    .box{
+        width: 200px;
+        height: 200px;
+        border: 1px #ccc solid ;
+    }
+</style>
